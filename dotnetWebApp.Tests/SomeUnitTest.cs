ea74738f48749b1c14ae870436bd72dcdeb68ad5
Dara Oladapo <daraoladapo@outlook.com>
2020-05-20 10:21:34 +0100

    +test

---
+++ dotnetWebApp.Tests/SomeUnitTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace dotnetWebApp.Tests
+{
+    public class SomeUnitTest
+    {
+        [Fact]
+        public void Food()
+        {
+            Assert.IsTrue(1==1);
+        }
+    }
+}
